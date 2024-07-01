c9d47828709d02ab8b9fcf5d6602e62ce25d5748
[root@gio-devops]$ <jpiedramacas@gmail.com>
2024-07-01 18:53:21 +0200

    v1a

--- main.tf
+++ main.tf
@@ -3,11 +3,9 @@ provider "aws" {
 }
 
 locals {
-  environment         = terraform.workspace
-  bucket_name         = terraform.workspace == "prod" ? "prod-example-bucket" : "dev-example-bucket"
-  instance_type       = terraform.workspace == "prod" ? "t2.small" : "t3.micro"
-  db_instance_class   = terraform.workspace == "prod" ? "db.t3.medium" : "db.t3.micro"
-  db_allocated_storage= terraform.workspace == "prod" ? 20 : 10
+  environment   = terraform.workspace
+  bucket_name   = terraform.workspace == "prod" ? "prod-example-bucket" : "dev-example-bucket"
+  instance_type = terraform.workspace == "prod" ? "t2.small" : "t2.micro"
 }
 
 resource "aws_instance" "example" {
@@ -16,22 +14,6 @@ resource "aws_instance" "example" {
 
   tags = {
     Name        = "${local.environment}-example-instance"
-    Environment = local.environment
-  }
-}
-
-resource "aws_db_instance" "example" {
-  allocated_storage    = local.db_allocated_storage
-  engine               = "mysql"
-  engine_version       = "8.0.35"
-  instance_class       = local.db_instance_class
-  db_name              = "exampledb"
-  username             = "admin"
-  password             = "password"
-  parameter_group_name = "default.mysql8.0"
-
-  tags = {
-    Name        = "${local.environment}-example-db"
     Environment = local.environment
   }
 }
